fdb76d85d5524fde95ea19f45f2bee1612a011cf
cg-at-ibm <130545587+cg-at-ibm@users.noreply.github.com>
2023-06-28 10:38:30 -0700

    Update __init__.py

--- plugins/operators/__init__.py
+++ plugins/operators/__init__.py
@@ -1,5 +1,5 @@
-from plugins.operators.stage_redshift import StageToRedshiftOperator
-from plugins.operators.create_tables import CreateTablesOperator
+from operators.stage_redshift import StageToRedshiftOperator
+from operators.create_tables import CreateTablesOperator
 from operators.load_fact import LoadFactOperator
 from operators.load_dimension import LoadDimensionOperator
 from operators.data_quality import DataQualityOperator
